72361c656af664dc59236a0dba701dbaac61ee6f
potsh <potsh@163.com>
2018-12-18 23:05:31 +0800

    commit @home

--- Assets/Scripts/Capacities.cs
+++ Assets/Scripts/Capacities.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Capacities //身体健康状况能力因子
 {
@@ -14,15 +13,15 @@ public class Capacities //身体健康状况能力因子
 
     public CapacityDef Manipulation;  // △ 依赖因素：上肢、意识   △ 影响其他：影响所有用到手的功法的效率
 
-    public CapacityDef Sounding;  // △ 依赖因素：下巴、颈部、意识
+    public CapacityDef Sounding;  // △ 依赖因素：下巴、颈部、意识   △ 影响其他：影响部分用声音的绝技的效率
 
-    public CapacityDef Eating;  // △ 依赖因素：下巴、颈部、意识
+    public CapacityDef Eating;  // △ 依赖因素：下巴、颈部、意识   △ 影响其他：影响吃药物和食物的时间
 
-    public CapacityDef Breathing;  // △ 依赖因素：肺、颈部、胸腔
+    public CapacityDef Breathing;  // △ 依赖因素：肺、颈部、胸腔   △ 影响其他：影响内力转化真气的速率、内力回复速率、影响意识、移动
 
-    public CapacityDef BloodFiltration;  // △ 依赖因素：肾脏、肝脏
+    public CapacityDef BloodFiltration;  // △ 依赖因素：肾脏、肝脏   △ 影响其他：
 
-    public CapacityDef BloodPumping;  // △ 依赖因素：心脏
+    public CapacityDef BloodPumping;  // △ 依赖因素：心脏  △ 影响其他：
 
     public CapacityDef Metabolism;  // △ 依赖因素：胃
 }
