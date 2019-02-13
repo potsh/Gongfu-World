3e7019f94eb2bfaf7b0dd70c4897d5ea2ddb29c9
potsh <potsh@163.com>
2019-02-13 11:42:42 +0800

    commit and push

--- git_commit_push.sh
+++ git_commit_push.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
 
-$computer=Get-WMIObject  Win32_ComputerSystem
-$Cname=$computer.name
 
 Msg=$1
+
 git add .
+
 if [ "$Msg" == "" ]
 then
-    git commit -m "co and push @ "+"$Cname" 
+    git commit -m "commit and push" 
 else 
     git commit -m "$Msg"
 fi
+
 git push
